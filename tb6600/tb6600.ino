f95ff8ec7728e46a052d2b69a96f520d4d5384f2
George Sotirchos <630r63.7555@gmail.com>
2019-09-05 13:11:35 +0300

    Add homing switches

--- tb6600/tb6600.ino
+++ tb6600/tb6600.ino
@@ -2,21 +2,22 @@
 const int TOTAL_STEPPERS = 3;
 const int STEPS_PER_REVOLUTION = 200;
 
-const int PUL_PIN[] = {3,  5,  6};
-const int DIR_PIN[] = {2,  4,  7};
-const int  EN_PIN[] = {8, 12, 13};
+const int PUL_PIN[] = { 3,  5,  6};
+const int DIR_PIN[] = { 2,  4,  7};
+const int  EN_PIN[] = { 8, 12, 13};
+const int  SW_PIN[] = {10, 10, 11};
 
 int   totalPulses[] = {10, 5, 3};
 float rot_speed     = 10; // rpm
 int   interval      = (1000.0*60.0/(rot_speed*STEPS_PER_REVOLUTION))/2.0;
 
 
-void initializePins() {
-    for (int stepper = 0; stepper < TOTAL_STEPPERS; stepper++) {
+void initializePins(steppers[]) {
+    for (int i = 0; i < sizeof(steppers); i++) {
         // set and initialize the pins
-        pinMode(PUL_PIN[stepper], OUTPUT);
-        pinMode(DIR_PIN[stepper], OUTPUT);
-        pinMode( EN_PIN[stepper], OUTPUT);
+        pinMode(PUL_PIN[steppers[i]], OUTPUT);
+        pinMode(DIR_PIN[steppers[i]], OUTPUT);
+        pinMode( EN_PIN[steppers[i]], OUTPUT);
     }
 }
 
@@ -28,10 +29,41 @@ void waitForSerial() {
     Serial.println("[Continuing]");
 }
 
-void motorSteps(int stepper, int totalSteps) {
-    Serial.print("\n-- Rotating motor ");
-    Serial.print(stepper + 1);
+void homeStepper(int steppers, int swNumber) {
+    Serial.println("\n-- Homing motor(s).");
+
+    // reverse rotation
+    for (int i = 0; i < sizeof(steppers); i++) {
+        digitalWrite(DIR_PIN[steppers[i]], LOW);
+    }
+
+    while(digitalRead(DIR_PIN[steppers[0]]) == LOW) {
+        if (digitalRead(SW_PIN[swNumber]) == LOW) {
+            // keep moving
+            moveSteppers(steppers, 1);
+        } else {
+            // normal rotation
+            for (int i = 0; i < sizeof(steppers); i++) {
+                digitalWrite(DIR_PIN[steppers[i]], HIGH);
+            }
+            delay(interval);
+
+            Serial.print("Motor(s) homed.")
+        }
+    }
+}
+
+
+void moveSteppers(int steppers[], int totalSteps) {
+    if (digitalRead(DIR_PIN[steppers[0]]) == LOW) {
+        char sign[] = "-";
+    } else {
+        char sign[] = "+";
+    }
+
+    Serial.print("\n-- Rotating motor(s) ");
     Serial.print(", by ");
+    Serial.print(sign);
     Serial.print(totalSteps);
     Serial.println(" steps.");
 
@@ -39,19 +71,24 @@ void motorSteps(int stepper, int totalSteps) {
     waitForSerial();
 
     for (int step = 0; step < totalSteps; step++) {
-        digitalWrite(DIR_PIN[stepper], HIGH);
+        for (int i = 0; i < sizeof(steppers); i++) {
+            digitalWrite(DIR_PIN[steppers[i]], HIGH);
 
-        digitalWrite(PUL_PIN[stepper], HIGH);
-        delay(interval);
+            digitalWrite(PUL_PIN[steppers[i]], HIGH);
+            delay(interval);
 
-        digitalWrite(PUL_PIN[stepper], LOW);
-        delay(interval);
+            digitalWrite(PUL_PIN[steppers[i]], LOW);
+            delay(interval);
+
+        }
 
         // show progress
         Serial.print('\r');
         Serial.print("step: ");
+        Serial.print(sign);
         Serial.print(step + 1);
         Serial.print("/");
+        Serial.print(sign);
         Serial.println(totalSteps);
     }
 }
@@ -60,7 +97,10 @@ void setup() {
     // start the serial
     Serial.begin(9600);
 
-    initializePins();
+    // select steppers
+    int steppers[3] = {0, 1, 2}
+
+    initializePins(steppers[]);
 }
 
 void loop() {
@@ -79,8 +119,9 @@ void loop() {
         Serial.println(" rpm");
 
         // Send steps to every stepper
-        for (int stepper = 0; stepper < TOTAL_STEPPERS; stepper++) {
-            motorSteps(stepper, totalPulses[stepper]);
+        // TODO
+        for (int i = 0; i < sizeof(steppers); i++) {
+            moveSteppers(stepper, totalPulses[steppers[i]]);
         }
 
         Serial.println("----------------------------------------");
