b4393e72bfb5e5d0da68ba0ae4ec965b820709a7
7555G <630r63.7555@gmail.com>
2019-09-06 19:10:15 +0300

    Fix homing

--- tb6600/tb6600.ino
+++ tb6600/tb6600.ino
@@ -1,86 +1,135 @@
-// define pins numbers
-const int TOTAL_STEPPERS = 3;
+#define LENGTH(array) ( sizeof(array)/sizeof(array[0]) )
+
+#define MOVE(steppers) { \
+    waitForSerial(); \
+    shiftArr(LENGTH(steppers), steppers, -1); \
+    moveSteppers(LENGTH(steppers), steppers, totalSteps[steppers[1]]); \
+    shiftArr(LENGTH(steppers), steppers, +1); \
+}
+
+#define HOME(steppers) { \
+    waitForSerial(); \
+    shiftArr(3, steppers, -1); \
+    homeSteppers(LENGTH(steppers), steppers); \
+    shiftArr(3, steppers, +1); \
+}
+
+
+// steppers
+const int VERT_BAR_STEPPERS[]   = {1, 2};
+const int HORIZ_STOP_STEPPERS[] = {3};
+
 const int STEPS_PER_REVOLUTION = 200;
 
+// pins numbers
 const int PUL_PIN[] = { 3,  5,  6};
 const int DIR_PIN[] = { 2,  4,  7};
 const int  EN_PIN[] = { 8, 12, 13};
 const int  SW_PIN[] = {10, 10, 11};
 
-int   totalPulses[] = {10, 5, 3};
-float rot_speed     = 10; // rpm
-int   interval      = (1000.0*60.0/(rot_speed*STEPS_PER_REVOLUTION))/2.0;
+// motion parameters
+int   totalSteps[] = {10, 10, 5};
+float rot_speed     = 1; // rpm
+int   interval      = 1000.0*60.0/(rot_speed*STEPS_PER_REVOLUTION)/2.0;
+
+const int vert_bar_steppers[]   = {1, 2};
 
 
-void initializePins(steppers[]) {
-    for (int i = 0; i < sizeof(steppers); i++) {
+void initializePins() {
+    for (int i = 0; i < LENGTH(PUL_PIN); i++) {
         // set and initialize the pins
-        pinMode(PUL_PIN[steppers[i]], OUTPUT);
-        pinMode(DIR_PIN[steppers[i]], OUTPUT);
-        pinMode( EN_PIN[steppers[i]], OUTPUT);
+        pinMode(PUL_PIN[i], OUTPUT);
+        pinMode(DIR_PIN[i], OUTPUT);
+        pinMode( EN_PIN[i], OUTPUT);
+        
+        digitalWrite(DIR_PIN[i], HIGH); // positive direction
     }
 }
 
 void waitForSerial() {
-    Serial.println("[Press Return key to continue]");
+    Serial.println("\n[Press Return key to continue]");
     while (Serial.available()) { Serial.read(); }
     while (!Serial.available()) {}
     //Serial.println(Serial.read());
     Serial.println("[Continuing]");
 }
 
-void homeStepper(int steppers, int swNumber) {
-    Serial.println("\n-- Homing motor(s).");
+void shiftArr(int arrayLength, int array[], int value) {
+    for (int i = 0; i < arrayLength; i++) {
+        array[i] += value;
+    }
+}
+
+void SerialPrintSteppers(int stepperCount, int steppers[]) {
+    Serial.print("[");
+    Serial.print(steppers[0] + 1);
+
+    for (int i = 1; i < stepperCount; i++) {
+        Serial.print(", ");
+        Serial.print(steppers[i] + 1);
+    }
+
+    Serial.print("]");
+}
+
+void homeSteppers(int stepperCount, int steppers[]) {
+    Serial.print("\n== Homing motor(s) ");
+    SerialPrintSteppers(stepperCount, steppers);
+    Serial.println(".");
 
     // reverse rotation
-    for (int i = 0; i < sizeof(steppers); i++) {
+    for (int i = 0; i < stepperCount; i++) {
         digitalWrite(DIR_PIN[steppers[i]], LOW);
     }
 
     while(digitalRead(DIR_PIN[steppers[0]]) == LOW) {
-        if (digitalRead(SW_PIN[swNumber]) == LOW) {
+        if (digitalRead(SW_PIN[steppers[0]]) == LOW) {
             // keep moving
-            moveSteppers(steppers, 1);
+            moveSteppers(stepperCount, steppers, 1);
+
+            //for (int i = 0; i < stepperCount; i++) {
+            //    digitalWrite(DIR_PIN[steppers[i]], HIGH);
+            //}
+            //delay(interval);
+
         } else {
             // normal rotation
-            for (int i = 0; i < sizeof(steppers); i++) {
+            for (int i = 0; i < stepperCount; i++) {
                 digitalWrite(DIR_PIN[steppers[i]], HIGH);
             }
             delay(interval);
 
-            Serial.print("Motor(s) homed.")
         }
     }
+
+    Serial.println("\n== Homing finished.");
 }
 
-
-void moveSteppers(int steppers[], int totalSteps) {
+void moveSteppers(int stepperCount, int steppers[], int totalSteps) {
+    char sign;
     if (digitalRead(DIR_PIN[steppers[0]]) == LOW) {
-        char sign[] = "-";
+        sign = '-';
     } else {
-        char sign[] = "+";
+        sign = '+';
     }
 
     Serial.print("\n-- Rotating motor(s) ");
-    Serial.print(", by ");
+    SerialPrintSteppers(stepperCount, steppers);
+    Serial.print(" by ");
     Serial.print(sign);
     Serial.print(totalSteps);
-    Serial.println(" steps.");
-
-    // wait for keypress
-    waitForSerial();
+    Serial.println(" step(s).");
 
     for (int step = 0; step < totalSteps; step++) {
-        for (int i = 0; i < sizeof(steppers); i++) {
-            digitalWrite(DIR_PIN[steppers[i]], HIGH);
-
+        for (int i = 0; i < stepperCount; i++) {
             digitalWrite(PUL_PIN[steppers[i]], HIGH);
-            delay(interval);
-
-            digitalWrite(PUL_PIN[steppers[i]], LOW);
-            delay(interval);
-
         }
+        delay(interval);
+
+        for (int i = 0; i < stepperCount; i++) {
+            digitalWrite(PUL_PIN[steppers[i]], LOW);
+        }
+        delay(interval);
 
         // show progress
         Serial.print('\r');
@@ -91,25 +140,22 @@ void moveSteppers(int steppers[], int totalSteps) {
         Serial.print(sign);
         Serial.println(totalSteps);
     }
+
+    Serial.println("-- Rotating finished.");
 }
 
 void setup() {
     // start the serial
     Serial.begin(9600);
 
-    // select steppers
-    int steppers[3] = {0, 1, 2}
-
-    initializePins(steppers[]);
+    // itialize pins
+    initializePins();
 }
 
 void loop() {
     Serial.flush();
 
     while(1) {
-        // wait for keypress
-        waitForSerial();
-
         // show info
         Serial.println("----------------------------------------");
         Serial.println("---------------- START -----------------");
@@ -118,16 +164,21 @@ void loop() {
         Serial.print(rot_speed);
         Serial.println(" rpm");
 
-        // Send steps to every stepper
-        // TODO
-        for (int i = 0; i < sizeof(steppers); i++) {
-            moveSteppers(stepper, totalPulses[steppers[i]]);
-        }
+        // home and move vertical bar's stepper pair
+        HOME(VERT_BAR_STEPPERS);
+        MOVE(VERT_BAR_STEPPERS);
+
+        // home and move horizontal panel stop's stepper pair
+        HOME(HORIZ_STOP_STEPPERS);
+        MOVE(HORIZ_STOP_STEPPERS);
 
         Serial.println("----------------------------------------");
         Serial.println("---------------- FINISH ----------------");
         Serial.println("----------------------------------------");
+
+        // wait for keypress
+        waitForSerial();
+
     }
 }
-
 
